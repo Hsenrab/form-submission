6d84f55f35d85a6f35212129bf30d77f010db764
HSenrab <hllewoh@gmail.com>
2022-05-30 22:18:07 +0100

    Commented out code - hello world

--- src/App.tsx
+++ src/App.tsx
@@ -1,105 +1,111 @@
 // ./src/App.tsx
 
-import React, { useState } from 'react';
+
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@mui/material/Button';
 import Path from 'path';
 import uploadFileToBlob, { isStorageConfigured } from './azure-storage-blob';
 
-const storageConfigured = isStorageConfigured();
+// const storageConfigured = isStorageConfigured();
 
-const App = (): JSX.Element => {
-  // all blobs in container
-  const [blobList, setBlobList] = useState<string[]>([]);
+ const App = (): JSX.Element => {
+//   // all blobs in container
+//   const [blobList, setBlobList] = useState<string[]>([]);
 
-  // current file to upload into container
-  const [fileSelected, setFileSelected] = useState(null);
-  const [name, setName] = useState(null);
+//   // current file to upload into container
+//   const [fileSelected, setFileSelected] = useState(null);
+//   const [name, setName] = useState(null);
 
-  // UI/form management
-  const [uploading, setUploading] = useState(false);
-  const [inputKey, setInputKey] = useState(Math.random().toString(36));
+//   // UI/form management
+//   const [uploading, setUploading] = useState(false);
+//   const [inputKey, setInputKey] = useState(Math.random().toString(36));
 
-  const onFileChange = (event: any) => {
-    // capture file into state
-    setFileSelected(event.target.files[0]);
-  };
+//   const onFileChange = (event: any) => {
+//     // capture file into state
+//     setFileSelected(event.target.files[0]);
+//   };
 
-  const onNameChange = (event: any) => {
-    // capture name
-    setName(event.target.name);
-  };
+//   const onNameChange = (event: any) => {
+//     // capture name
+//     setName(event.target.value);
+//   };
 
-  const onFileUpload = async () => {
-    // prepare UI
-    setUploading(true);
+//   const onFileUpload = async () => {
+//     // prepare UI
+//     setUploading(true);
 
-    // Prep file
-    const filename = name ?? "unknown"
-    const cleanFileName = filename.replace(/\s/g, "");
+//     // Prep file
+//     const filename = name ?? "unknown"
+//     const cleanFileName = filename.replace(/\s/g, "");
 
 
 
-    var file = fileSelected ?? new File([""], "filename");
+//     var file = fileSelected ?? new File([""], "filename");
 
-    const fileExtension = file.name.split(".").pop()
+//     const fileExtension = file.name.split(".").pop()
 
-    var blob = file.slice(0, file.size, file.type); 
-    var newFile = new File([blob], cleanFileName.concat(".", fileExtension!), {type: file.type});
+//     var blob = file.slice(0, file.size, file.type); 
+//     var newFile = new File([blob], cleanFileName.concat(".", fileExtension!), {type: file.type});
 
-    // *** UPLOAD TO AZURE STORAGE ***
-    const blobsInContainer: string[] = await uploadFileToBlob(newFile);
+//     // *** UPLOAD TO AZURE STORAGE ***
+//     const blobsInContainer: string[] = await uploadFileToBlob(newFile);
 
-    // prepare UI for results
-    setBlobList(blobsInContainer);
+//     // prepare UI for results
+//     setBlobList(blobsInContainer);
 
-    // reset state/form
-    setFileSelected(null);
-    setUploading(false);
-    setInputKey(Math.random().toString(36));
-  };
+//     // reset state/form
+//     setFileSelected(null);
+//     setUploading(false);
+//     setInputKey(Math.random().toString(36));
+//   };
 
-  // display form
-  const DisplayForm = () => (
-    <div>
-      <input type="text" onChange={onNameChange}/>
-      <input type="file" onChange={onFileChange} key={inputKey || ''} />
-      <button type="submit" onClick={onFileUpload}>
-        Upload!
-          </button>
-    </div>
-  )
+//   // display form
+//   const DisplayForm = () => (
+//     <div>
+//       <input type="text" id="Guide" onChange={onNameChange}/>
+//       <input type="file" onChange={onFileChange} key={inputKey || ''} />
+//       <button type="submit" onClick={onFileUpload}>
+//         Upload!
+//           </button>
+//     </div>
+//   )
 
-  // display file name and image
-  const DisplayImagesFromContainer = () => (
-    <div>
-      <h2>Container items</h2>
-      <ul>
-        {blobList.map((item) => {
-          return (
-            <li key={item}>
-              <div>
-                {Path.basename(item)}
-                <br />
-                <img src={item} alt={item} height="200" />
-              </div>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
-  );
+//   // display file name and image
+//   const DisplayImagesFromContainer = () => (
+//     <div>
+//       <h2>Container items</h2>
+//       <ul>
+//         {blobList.map((item) => {
+//           return (
+//             <li key={item}>
+//               <div>
+//                 {Path.basename(item)}
+//                 <br />
+//                 <img src={item} alt={item} height="200" />
+//               </div>
+//             </li>
+//           );
+//         })}
+//       </ul>
+//     </div>
+//   );
 
-  return (
-    <div>
-      <h1>Painswick Beacon</h1>
-      {storageConfigured && !uploading && DisplayForm()}
-      {storageConfigured && uploading && <div>Uploading</div>}
-      <hr />
-      {storageConfigured && blobList.length > 0 && DisplayImagesFromContainer()}
-      {!storageConfigured && <div>Storage is not configured.</div>}
-    </div>
-  );
-};
+   return (
+    <Button variant="contained" color="primary">
+    Hello World
+    </Button>
+//     <div>
+//       <h1>Painswick Beacon</h1>
+//       {storageConfigured && !uploading && DisplayForm()}
+//       {storageConfigured && uploading && <div>Uploading</div>}
+//       <hr />
+//       {storageConfigured && blobList.length > 0 && DisplayImagesFromContainer()}
+//       {!storageConfigured && <div>Storage is not configured.</div>}
+//     </div>
+   );
+ };
 
-export default App;
+ export default App;
 
 
